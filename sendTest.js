b762ec4d7eeb5739230d78811fbfa593676f4473
Hritika Sharan <117531823+hritika2409@users.noreply.github.com>
2025-05-18 21:03:36 +0530

    Update sendTest.js

--- sendTest.js
+++ sendTest.js
@@ -1,42 +1,28 @@
 const amqp = require('amqplib');
-require('dotenv').config();
 
-function getPriorityValue(priority) {
-  switch (priority) {
-    case 'critical': return 10;
-    case 'normal': return 5;
-    case 'low': return 1;
-    default: return 5;
+const RABBITMQ_URL = 'amqps://izoabvou:jClP9z69xCw7HuU8uH1klAz_hA9DIlsj@puffin.rmq2.cloudamqp.com/izoabvou';
+
+(async () => {
+  try {
+   
+    const connection = await amqp.connect(RABBITMQ_URL);
+    const channel = await connection.createChannel();
+
+  
+    const queue = 'testQueue';
+    await channel.assertQueue(queue, {
+      durable: false
+    });
+
+    const message = 'Hey Its important!';
+    channel.sendToQueue(queue, Buffer.from(message));
+    console.log('✅ Message sent:', message);
+
+    setTimeout(() => {
+      connection.close();
+      process.exit(0);
+    }, 500);
+  } catch (error) {
+    console.error('❌ Error sending message:', error);
   }
-}
-
-async function sendTestMessage() {
-  const connection = await amqp.connect(process.env.RABBITMQ_URL);
-  const channel = await connection.createChannel();
-  const queue = 'notifications';
-
-  await channel.assertQueue(queue, {
-    durable: true,
-    maxPriority: 10,
-  });
-
-  const msg = {
-    userId: '123',
-    type: 'email',
-    priority: 'low',
-    message: 'Hey',
-  };
-
-  channel.sendToQueue(queue, Buffer.from(JSON.stringify(msg)), {
-    persistent: true,
-    priority: getPriorityValue(msg.priority),
-  });
-
-  console.log('✅ Sent message with priority:', msg.priority);
-  setTimeout(() => {
-    connection.close();
-    process.exit(0);
-  }, 500);
-}
-
-sendTestMessage();
+})();
